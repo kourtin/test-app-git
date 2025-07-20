fb3353a9b2c9f2be1b87420ceeb1a77c02f008ba
kourtin <kourtin_ismahane.math@yahoo.fr>
2025-07-20 02:52:44 +0200

    Version 9 sur la branche master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <h1>Version 3</h1>
         <h1>Version 4</h1>
         <h1>Version 5 from Github</h1>
-        <h1>Version 6 en local</h1>
+        <h1>Version 6 en local</h1>*<h1>Version 9 sur la branche master</h1>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -30,7 +30,6 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
